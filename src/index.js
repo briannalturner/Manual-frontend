86571461f97c4775b31848436a3b134392d2e446
briannalturner <blturner3527@gmail.com>
2020-03-24 14:48:21 -0400

    added cropping and new modals

--- src/index.js
+++ src/index.js
@@ -4,25 +4,45 @@ document.addEventListener("DOMContentLoaded", () => {
     homeBtn.addEventListener("click", landingPage)
     landingPage()
 
-    var modal = document.getElementById("myModal");
-    var btn = document.getElementById("login-button");
-    var span = document.getElementsByClassName("close")[0];
+    createLoginModal()
+    createNewUserModal()
+    
+    let loginForm = document.getElementById('login-form')
+    loginForm.addEventListener("submit", loginHandler)
+
+    let createUserRequest = document.getElementById('create-new-user')
+    createUserRequest.addEventListener("click", createUserHandler)
+
+    let allUserBtn = document.getElementById('all-users')
+    allUserBtn.addEventListener("click", fetchAllUsers)
+
+    let allPostBtn = document.getElementById('all-posts')
+    allPostBtn.addEventListener("click", fetchAllPosts)
+
+})
+
+function createLoginModal() {
+    let loginModal = document.getElementById("myModal");
+    let btn = document.getElementById("login-button");
+    let span = document.getElementsByClassName("close")[0];
 
     btn.onclick = function() {
-        modal.style.display = "block";
+        loginModal.style.display = "block";
     }
     span.onclick = function() {
-        modal.style.display = "none";
+        loginModal.style.display = "none";
     }
     window.onclick = function(event) {
-        if (event.target == modal) {
-          modal.style.display = "none";
+        if (event.target == loginModal) {
+          loginModal.style.display = "none";
         }
     }
+}
 
-    var createUserModal = document.getElementById("createUserModal");
-    var createUserbtn = document.getElementById("create-new-user");
-    var createSpan = document.getElementsByClassName("close")[1];
+function createNewUserModal() {
+    let createUserModal = document.getElementById("createUserModal");
+    let createUserbtn = document.getElementById("create-new-user");
+    let createSpan = document.getElementsByClassName("close")[1];
     
     createUserbtn.onclick = function() {
         createUserModal.style.display = "block";
@@ -35,21 +55,32 @@ document.addEventListener("DOMContentLoaded", () => {
           createUserModal.style.display = "none";
         }
     }
+}
+
+function createNewPostModal() {
+    let createPostModal = document.getElementById("createPostModal");
+    let createPostbtn = document.getElementById("new-post");
+    let createSpan = document.getElementsByClassName("close")[2];
     
-    let loginForm = document.getElementById('login-form')
-    loginForm.addEventListener("submit", loginHandler)
-
-    let createUserRequest = document.getElementById('create-new-user')
-    createUserRequest.addEventListener("click", createUserHandler)
-
-    let allUserBtn = document.getElementById('all-users')
-    allUserBtn.addEventListener("click", fetchAllUsers)
-
-})
+    createPostbtn.onclick = function() {
+        createPostModal.style.display = "block";
+    }
+    createSpan.onclick = function() {
+        createPostModal.style.display = "none";
+    }
+    window.onclick = function(event) {
+        if (event.target == createPostModal) {
+          createPostModal.style.display = "none";
+        }
+    }
+}
 
 function fetchNewPosts() {
-    fetch("http://localhost:3000/posts/?_limit=4").then(resp => resp.json())
-        .then(posts => posts.forEach(post => renderPost(post)))
+    fetch("http://localhost:3000/posts").then(resp => resp.json())
+        .then(posts => {
+            posts = posts.slice(-4)
+            posts.forEach(post => renderPost(post))
+        })
 }
 
 function renderPost(post) {
@@ -95,13 +126,14 @@ function renderPost(post) {
 }
 
 function showPost(post) {
-    console.log('click')
+    console.log("hit")
     // creating postContainer
     let postContainer = document.createElement('span')
     let showContainer = findShowContainer()
+    console.log(showContainer)
     let mainContainer = findMainContainer()
-    mainContainer.innerHTML = ""
     showContainer.innerHTML = ""
+    mainContainer.innerHTML = ""
     mainContainer.classList.remove('parallax')
     showContainer.append(postContainer)
     postContainer.classList.add('show-content')
@@ -131,11 +163,6 @@ function findShowContainer() {
     return document.getElementsByClassName('show-container')[0]
 }
 
-// function logged_in() {
-//     fetch()
-//     console.log(sessionStorage.setItem('key', 'value'))
-// }
-
 function loginHandler(event) {
     let username = event.target.username_input.value
     let password = event.target.password_input.value
@@ -157,30 +184,45 @@ function renderAllUsers(users) {
     let mainContainer = findMainContainer()
     let showContainer = findShowContainer()
     let userContainer = document.createElement('div')
-    userContainer.classList.add('show-content')
+    let header = document.createElement('h1')
+
+    header.innerText = "Explore Users"
+    header.style.margin = '10px'
+    header.classList.add('center')
 
     showContainer.innerHTML = ""
     mainContainer.innerHTML = ""
     mainContainer.classList.remove('parallax')
+    userContainer.classList.add('card-columns')
+    userContainer.classList.add('users-container')
+    userContainer.style.paddingTop = '15px'
 
     users.forEach(user => userContainer.append(createUserDiv(user)))
-    showContainer.append(userContainer)
+    mainContainer.append(header, userContainer)
 }
 
 function createUserDiv(user) {
-    let div = document.createElement('div')
-    let fullName = document.createElement('h5')
-    let age = document.createElement('p')
-    let username = document.createElement('p')
-    let ageUsernameSpan = document.createElement('span')
-    ageUsernameSpan.append(age, username)
-    div.append(fullName, ageUsernameSpan)
+    let userCard = document.createElement('div')
 
-    fullName.innerText = user.first_name + " " + user.last_name
-    age.innerText = "Age: " + user.age
-    username.innerText = "Username: " + user.username
-
-    return div
+    userCard.innerHTML = `
+        <div class="card">
+            <div class="card-header">@${user.username}</div>
+            <div class="card-body">
+                <span>
+                    <div>
+                        <img class="show-user-picture" src="${user.image}">
+                    </div>
+                    <hr>
+                    <div id="post-body">
+                        <h5 class="card-title"">${user.first_name} ${user.last_name}</h5>
+                        <p class="card-text">${user.bio}</p>
+                    </div>
+                </span>
+            </div>
+        </div>
+    `
+    userCard.addEventListener("click", () => showUser(user))
+    return userCard 
 }
 
 function createUserHandler(event) {
@@ -188,4 +230,65 @@ function createUserHandler(event) {
     modal.style.display = "none"
 
     let createForm = document.getElementById('createUserModal')
+}
+
+function fetchAllPosts() {
+    fetch("http://localhost:3000/posts").then(resp => resp.json())
+        .then(posts => renderPosts(posts))
+}
+
+function renderPosts(posts) {
+    let mainContainer = findMainContainer()
+    let showBox = findShowContainer()
+    let postsContainer = document.createElement('div')
+    
+    mainContainer.classList.remove('parallax')
+    postsContainer.classList.add('card-columns')
+    postsContainer.classList.add('posts-container')
+    postsContainer.style.paddingTop = '15px'
+    showBox.innerHTML =""
+    mainContainer.innerHTML = ""
+
+    let header = document.createElement('h1')
+    let p = document.createElement('p')
+    header.innerText = "Explore Posts"
+    p.innerHTML = `
+        or <a href="#" id="new-post">write a new one</a>
+    `
+    header.style.margin = '10px'
+    header.classList.add('center')
+    
+    posts.forEach(post => postsContainer.append(makePostCard(post)))
+    
+    mainContainer.append(header, p, postsContainer)
+    createNewPostModal()
+}
+
+function makePostCard(post) {
+    let card = document.createElement('div')
+
+    card.innerHTML = `
+        <div class="card">
+            <div class="card-header">Posted ${post.created_at.substring(0,10)}</div>
+            <div class="card-body">
+                <span>
+                    <div>
+                        <img class="post-card-images" src="${post.image}">
+                    </div>
+                    <hr>
+                    <div id="post-body">
+                        <h5 class="card-title"">${post.subject}</h5>
+                        <p class="card-text">${post.body.substr(0,200)}...</p>
+                    </div>
+                </span>
+            </div>
+        </div>
+    `
+
+    card.addEventListener("click", () => showPost(post))
+    return card
+}
+
+function showUser(user) {
+    console.log("I need to be defined bri")
 }
