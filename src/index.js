dca7bd6a314d4859af7f0ab595478b85b03017e2
briannalturner <blturner3527@gmail.com>
2020-03-26 01:18:18 -0400

    added update user modal

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
+    localStorage.clear()
     fetchNewPosts()
     landingPage()
 
@@ -83,7 +84,10 @@ function saveNewUser(event) {
             "Accept": "application/json"
         },
         body: JSON.stringify(payload)
-    }).then(resp => resp.json()).then(user => userContainer.append(createUserDiv(user)))
+    }).then(resp => resp.json()).then(user => {
+        setLoggedIn(user)
+        userContainer.append(createUserDiv(user))
+    })
 
     event.target.reset()
     event.target.parentNode.parentNode.style.display = "none";
@@ -97,16 +101,32 @@ function createNewPostModal() {
     let createSpan = document.getElementsByClassName("close")[2];
     let form = document.getElementById('create_post_form')
     form.addEventListener("submit", newPostHandler)
-    
+        
     createPostbtn.onclick = function() {
-        createPostModal.style.display = "block";
+        if (localStorage.id !== null && localStorage.id !== undefined) {
+            createPostModal.style.display = "block";
+        } else {
+            let loginModal = document.getElementById("myModal");
+            let span = document.getElementsByClassName("close")[0];
+    
+            loginModal.style.display = "block";
+    
+            span.onclick = function() {
+                loginModal.style.display = "none";
+            }
+            window.onclick = function(event) {
+                if (event.target == loginModal) {
+                loginModal.style.display = "none";
+                }
+            }
+        }
     }
     createSpan.onclick = function() {
         createPostModal.style.display = "none";
     }
     window.onclick = function(event) {
         if (event.target == createPostModal) {
-          createPostModal.style.display = "none";
+        createPostModal.style.display = "none";
         }
     }
 }
@@ -118,7 +138,7 @@ function newPostHandler(event) {
     let url = event.target.create_url.value
     let postsContainer = document.getElementsByClassName('posts-container')[0]
 
-    let payload = {user_id: 6, body: body, subject: subject, image: url}
+    let payload = {user_id: localStorage.id, body: body, subject: subject, image: url}
 
     fetch("http://localhost:3000/posts", {
         method: "POST",
@@ -131,7 +151,6 @@ function newPostHandler(event) {
         postsContainer.append(makePostCard(post))
         fetchNewPosts()
     })
-
     event.target.reset()
     event.target.parentNode.parentNode.style.display = "none";
     let btn = document.getElementById("login-button");
@@ -168,6 +187,7 @@ function createPostCard(post) {
     card.style.width = "90%"
     card.addEventListener("click", () => showPost(post))
 
+    // console.log(post)
     // if post has an image, add it
     if (post.image !== null){
         let image = document.createElement('img')
@@ -200,7 +220,6 @@ function showPost(post) {
     // creating postContainer
     let postContainer = document.createElement('span')
     let showContainer = findShowContainer()
-    console.log(showContainer)
     let mainContainer = findMainContainer()
     showContainer.innerHTML = ""
     mainContainer.innerHTML = ""
@@ -208,24 +227,104 @@ function showPost(post) {
     showContainer.append(postContainer)
     showContainer.classList.add('show-content')
 
-    let commentsContainer = document.createElement('div')
-    fetch("http://localhost:3000/comments")
-    .then(resp => resp.json())
-    .then(comments => comments.filter(comment => console.log(comment)))
-
+    // console.log(post.user)
     // adding content to postContainer
     let postTitle = document.createElement('h2')
+    let postAuthor = document.createElement('h5')
+    postAuthor.innerText = post.user.first_name + " " + post.user.last_name
     postTitle.innerText = post.subject
     postContainer.append(postTitle, postAuthor)
 
+    let hr = document.createElement('hr')
+    commentsContainer = addComments(post)
+
     let postContent = document.createElement('p')
     postContent.innerText = post.body
-    postContainer.append(postContent)
+    postContainer.append(postContent, hr, commentsContainer)
+}
+
+function addComments(post) {
+    let commentForm = document.createElement('form')
+    let textArea = document.createElement('textarea')
+    let submitBtn = document.createElement('button')
+    submitBtn.innerText = "Post"
+    submitBtn.classList.add('btn-secondary', 'rounded')
+    submitBtn.type = "submit"
+    submitBtn.style.marginBottom = "15px"
+    textArea.style.marginBottom = "15px"
+    textArea.id = "text_area"
+    textArea.placeholder = "write a comment..."
+    textArea.classList.add('form-control','col-md-5')
+    commentForm.append(textArea, submitBtn)
+
+    commentForm.addEventListener("submit", () => newCommentHandler(post))
+
+    let commentsContainer = document.createElement('div')
+    commentsContainer.id = "comments-container"
+    let commentHeader = document.createElement('h4')
+    commentHeader.innerText = "Comments"
+    commentsContainer.append(commentHeader, commentForm)
+    post.comments.forEach(comment => commentsContainer.append(renderComment(comment)))
+    return commentsContainer
 }
 
 function renderComment(comment) {
-    let commentDiv = document.createElement('div')
-    commentDiv.innerText = comment.body
+    let commentLi = document.createElement('div')
+
+    commentLi.innerHTML = `
+        <div class="card mb-3" style="max-width: 540px;">
+            <div class="row no-gutters">
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-text" style="font-size: 16px">@${comment.user.username}</h5>
+                        <p class="card-text" style="font-size: 13px">${comment.body}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `
+    commentLi.addEventListener("click", () => showUser(comment.user))
+
+    return commentLi
+}
+
+function newCommentHandler(post) {
+    if (localStorage.id !== null && localStorage.id !== undefined) {
+        let comment_body = event.target.text_area.value
+        let user_id = localStorage.id
+        let post_id = post.id
+        let payload = {body: comment_body, user_id: user_id, post_id: post_id}
+
+        fetch('http://localhost:3000/comments', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": 'application/json'
+            },
+            body: JSON.stringify(payload)
+        })
+        .then(resp => resp.json())
+        .then(comment => {
+            let commentsContainer = document.getElementById('comments-container')
+            commentsContainer.append(renderComment(comment))
+        })
+
+        event.target.reset()
+    } else {
+        let loginModal = document.getElementById("myModal");
+        let span = document.getElementsByClassName("close")[0];
+
+        loginModal.style.display = "block";
+
+        span.onclick = function() {
+            loginModal.style.display = "none";
+        }
+        window.onclick = function(event) {
+            if (event.target == loginModal) {
+            loginModal.style.display = "none";
+            }
+        }
+    }
 }
 
 function landingPage() {
@@ -239,6 +338,7 @@ function landingPage() {
 function findMainContainer() {
     return document.getElementsByClassName('main-container')[0]
 }
+
 function findShowContainer() {
     showContainer = document.getElementsByClassName('show-container')[0]
     showContainer.classList.remove('show-content')
@@ -249,33 +349,35 @@ function loginHandler(event) {
     let username = event.target.username_input.value
     let password = event.target.password_input.value
 
-    let loggedInName = document.getElementById('logged-in-as')
-    loggedInName.innerText = username
-    loggedInName.addEventListener("click", showUser())
+    let deleteNode = event.target.parentNode.parentNode
 
-    // fetch("http://localhost:3000/users").then(resp => resp.json())
-    //     .then(usersArray => {
-    //         user = usersArray.filter(user => user.username == username)
-    //         if (user){
-    //             setLoggedIn(user)
-    //         } else {
-    //             alert("That user doesn't exist in our database")
-    //         }
-    //     })
-            
-    event.target.reset()
-    event.target.parentNode.parentNode.style.display = "none";
-    let btn = document.getElementById("login-button");
-    btn.hidden = true
+    fetch("http://localhost:3000/users").then(resp => resp.json())
+    .then(usersArray => {
+        let user = usersArray.filter(user => {
+            return user.username == username && user.password_digest == password
+        })[0]
+        if (user !== undefined){
+            setLoggedIn(user)
+            deleteNode.style.display = "none";
+            let btn = document.getElementById("login-button");
+            btn.hidden = true
+            let loggedInName = document.getElementById('logged-in-as')
+            loggedInName.innerText = user.first_name + " " + user.last_name
+            loggedInName.addEventListener("click", () => showUser(user))
+        } else {
+            alert("We couldn't find a user with those credentials")
+            event.target.reset()
+        }
+    })
 }
 
-// function setLoggedIn(user) {
-//     let loggedInName = document.getElementById('logged-in-as')
-//     loggedInName.innerText = user.username
-//     loggedInName.addEventListener("click", showUser())
-//     localStorage.id = user.id
-//     localStorage.username = user.username
-// }
+function setLoggedIn(user) {
+    let loggedInName = document.getElementById('logged-in-as')
+    loggedInName.innerText = user.first_name + " " + user.last_name
+    loggedInName.addEventListener("click", () => showUser(user))
+    localStorage.setItem('id', user.id);
+    localStorage.setItem('username', user.username);
+}
 
 function fetchAllUsers() {
     fetch('http://localhost:3000/users').then(resp => resp.json())
@@ -366,7 +468,9 @@ function renderPosts(posts) {
 
     header.classList.add('center')
     
-    posts.forEach(post => postsContainer.append(makePostCard(post)))
+    posts.forEach(post => {
+        postsContainer.append(makePostCard(post))
+    })
     
     mainContainer.append(header, p, postsContainer)
     createNewPostModal()
@@ -392,11 +496,110 @@ function makePostCard(post) {
             </div>
         </div>
     `
-
+    console.log(post)
     card.addEventListener("click", () => showPost(post))
     return card
 }
 
-function showUser(user) {
-    console.log("I need to be defined bri")
+function showUser(user) { 
+    // creating userContainer
+    let userContainer = document.createElement('span')
+    let showContainer = findShowContainer()
+    let mainContainer = findMainContainer()
+    userContainer.innerHTML = ""
+    showContainer.innerHTML = ""
+    mainContainer.innerHTML = ""
+    mainContainer.classList.remove('parallax')
+    showContainer.append(userContainer)
+    showContainer.classList.add('show-content')
+
+    let fullName = user.first_name + " " + user.last_name
+    let postsLength
+    if (user.posts !== undefined || user.posts !== null) {
+        postsLength = user.posts.length
+    } else {
+        postsLength = 0
+    }
+
+    let container = document.createElement('div')
+    container.innerHTML = `
+        <div class="card mb-3">
+            <img src="${user.image}" class="card-img-top show-user-picture" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${fullName}</h5>
+                <p class="card-text">${user.bio}</p>
+                <p class="card-text"><small class="text-muted">${postsLength} posts</small></p>
+            </div>
+        </div>
+    `
+
+    if (user.id == localStorage.id) {
+        let span = document.createElement('span')
+        span.classList.add('row')
+        span.style.marginLeft = '5px'
+        let editBtn = document.createElement('button')
+        editBtn.innerText = "Edit Profile"
+        editBtn.classList.add('btn-secondary', 'rounded')
+        editBtn.style.margin = '5px'
+        editBtn.addEventListener("click", () => editUserHandler(user))
+        span.append(editBtn)
+
+        container.append(span)
+    }
+
+    userContainer.append(container)
+}
+
+function editUserHandler(user) {
+    let updateUserModal = document.getElementById("updateUserModal");
+    let updateSpan = document.getElementsByClassName("close")[1];
+    let form = document.getElementById('update-user-form')
+    form.addEventListener("submit", () => saveUpdatedUser(user))
+    form.update_username.value = user.username
+    form.update_password.value = user.password_digest
+    form.update_bio.value = user.bio
+    form.update_first_name.value = user.first_name
+    form.update_last_name.value = user.last_name
+    form.update_age.value = user.age
+    form.update_image.value = user.image
+
+    
+    updateUserModal.style.display = "block";
+    updateSpan.onclick = function() {
+        updateUserModal.style.display = "none";
+    }
+    window.onclick = function(event) {
+        if (event.target == updateUserModal) {
+          updateUserModal.style.display = "none";
+        }
+    }
+}
+
+function saveUpdatedUser(user) {
+    let firstName = event.target.update_first_name.value
+    let lastName = event.target.update_last_name.value
+    let age = event.target.update_age.value
+    let username = event.target.update_username.value
+    let password = event.target.update_password.value
+    let bio = event.target.update_bio.value
+    let image = event.target.update_image.value
+
+
+    let payload = {first_name: firstName, last_name: lastName, age: age, username: username, password_digest: password, bio: bio, image: image}
+
+    fetch(`http://localhost:3000/users/${user.id}`, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(payload)
+    }).then(resp => resp.json()).then(user => {
+        setLoggedIn(user)
+        showUser(user)
+
+    })
+
+    event.target.reset()
+    event.target.parentNode.parentNode.style.display = "none";
 }
